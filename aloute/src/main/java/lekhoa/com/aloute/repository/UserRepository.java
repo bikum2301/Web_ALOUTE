1100cdcc7975f388c59cb86a3b50a3cfcc91279d
bikum2301 <phuongtdp2301@gmail.com>
2024-12-05 11:22:13 +0700

    Delete aloute directory

--- aloute/src/main/java/lekhoa/com/aloute/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package lekhoa.com.aloute.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import lekhoa.com.aloute.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-	UserEntity findByUsername(String username);
-}
